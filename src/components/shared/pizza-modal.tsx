9caccb269f8711db974424daf7dfdbab3f67f2bd
Anastasia <158266030+Anastasiya-Stavrova@users.noreply.github.com>
2025-02-06 11:26:40 +0700

    Добавлен адаптив к модальному окну

--- src/components/shared/pizza-modal.tsx
+++ src/components/shared/pizza-modal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import toast from "react-hot-toast";
+import { useMedia } from "react-use";
 
 import { cn } from "@/lib";
 import { BasketItemState, useAuth, useBasketActions } from "@/context";
@@ -10,6 +11,7 @@ import {
   mapPizzaSizeToNumber,
   pizzaSizeNames,
   mapPizzaIngredientsToName,
+  ICONS,
 } from "@/constants";
 
 import {
@@ -17,6 +19,9 @@ import {
   Dialog,
   DialogContent,
   DialogTitle,
+  Drawer,
+  DrawerContent,
+  DrawerTitle,
   IngredientItem,
   PizzaImage,
   PizzaInfoPopover,
@@ -42,6 +47,8 @@ export const PizzaModal = ({
   const { authToken } = useAuth();
   const { addBasketItem, updateSelectedItem } = useBasketActions();
   const { states, functions } = usePizzaOptions(pizza, item);
+
+  const isSmallDevice = useMedia("(max-width: 768px)", true);
 
   const handleClickAdd = () => {
     if (!authToken) {
@@ -104,6 +111,119 @@ export const PizzaModal = ({
 
     onClickOpenChange();
   };
+
+  if (isSmallDevice) {
+    return (
+      <Drawer
+        open={isOpen}
+        onOpenChange={() => {
+          handleChange();
+        }}
+      >
+        <DrawerTitle className="hidden" />
+        <DrawerContent className={cn("w-full h-full", className)}>
+          <div className="w-full h-full flex flex-col">
+            <div className="w-full flex gap-4 p-4 items-center">
+              <button
+                className="cursor-pointer w-fit h-fit p-0"
+                onClick={() => {
+                  handleChange();
+                }}
+              >
+                <img src={ICONS.ARROW} alt="Arrow" className="size-8" />
+              </button>
+              <Typography text="Пицца" size="xl" />
+            </div>
+
+            <div className="w-full flex items-center justify-center h-[220px] min-h-[220px]">
+              <PizzaImage
+                name={pizza.name}
+                src={process.env.NEXT_PUBLIC_API_URL + pizza.img}
+                size={mapPizzaSizeToNumber[states.size]}
+              />
+            </div>
+
+            <div className="w-full h-full flex flex-col gap-6 p-4 items-center justify-between mt-8 overflow-y-auto scrollbar">
+              <div className="flex flex-col gap-6 w-full px-4">
+                <div className="flex flex-col gap-2">
+                  <div className="w-full flex gap-3 items-center justify-between">
+                    <Typography
+                      text={pizza.name}
+                      size="xl"
+                      className="text-[#292929]"
+                    />
+
+                    <PizzaInfoPopover
+                      calories={pizza.calories}
+                      protein={pizza.protein}
+                      totalFat={pizza.totalFat}
+                      carbohydrates={pizza.carbohydrates}
+                    />
+                  </div>
+
+                  <Typography
+                    text={`${
+                      mapPizzaSizeToNumber[states.size]
+                    } см, традиционное тесто`}
+                    className="text-[#535353]"
+                  />
+
+                  <Typography
+                    text={getPizzaIngredientsDetails(pizza.ingredients)}
+                    size="md"
+                    className="text-[#535353]"
+                  />
+                </div>
+
+                <ToggleGroup
+                  className="w-full mx-auto"
+                  items={pizzaSizeNames}
+                  value={String(states.size)}
+                  onClick={value => functions.setSize(value as PizzaSizeName)}
+                />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <Typography
+                  text=" Добавить по вкусу"
+                  size="md"
+                  className="font-medium px-4"
+                />
+
+                <div className="w-full p-4">
+                  <div className="grid grid-cols-2 sm:grid-cols-3 gap-[11px] h-full">
+                    {pizza.toppings.map(topping => (
+                      <IngredientItem
+                        key={topping.name}
+                        name={capitalizeFirstLetter(
+                          mapPizzaIngredientsToName[topping.name]
+                        )}
+                        imageUrl={process.env.NEXT_PUBLIC_API_URL + topping.img}
+                        price={topping.cost}
+                        active={states.selectedToppings.has(topping.name)}
+                        onClick={() => functions.addToppings(topping.name)}
+                      />
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-full h-full max-h-[128px] shadow-lg shadow-black/5 bg-background py-4 flex justify-center">
+              <Button
+                onClick={() => handleClickAdd()}
+                className="w-[80%] max-w-[350px]"
+              >
+                {item
+                  ? `Изменить выбранный товар за ${states.totalPrice} ₽`
+                  : `Добавить в корзину за ${states.totalPrice} ₽`}
+              </Button>
+            </div>
+          </div>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
 
   return (
     <Dialog
--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -1,4 +1,5 @@
 export const ICONS = {
   QUESTION: "/assets/icons/question.svg",
   HELP: "/assets/icons/help.svg",
+  ARROW: "/assets/icons/arrow.svg",
 };
